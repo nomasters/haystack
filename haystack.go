aeba32d50c0898abb85c49b8a70a9671761998b4
Nathan Toups <n@rjrbt.io>
2022-01-31 23:57:42 -0700

    POC wip

--- haystack.go
+++ haystack.go
@@ -32,3 +32,29 @@ var (
 // --- if valid, write the payload to storage
 // --- return the the hash(write hash + TTL)
 // - create a storage interface for reads and writes
+
+/*
+Ideas for how the code works for the client.
+
+
+// The client should configure a UDP connection and handle interacting with the server
+client, err := haystack.New("localhost:8080")
+
+// if we want to pass in options, we can do it like this.
+client, err := haystack.New("localhost:8080", ...opts)
+
+// with the client we can read and write to the haystack server
+// it returns a needle and an error
+needle, err := client.Get(key)
+
+// to create a needle you'd write, the message must be _exactly_ 448 bytes, or an error is returned
+// this creates a needle which includes the blake2b 256 hash.
+needle, err := needle.New(message)
+// FromBytes creates a needle from 480 raw bytes.
+needle, err := needle.FromBytes(b)
+
+// posting to haystack takes a single argument of a needle
+err := client.Post(needle)
+
+
+*/
