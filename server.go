3496f057b7dcefaa2c537f43cbafc624bb5a402e
Nathan Toups <n@rjrbt.io>
2022-02-03 22:08:23 -0700

    feat: outline server

--- server.go
+++ server.go
@@ -1,1 +1,8 @@
 package haystack
+
+// this will be the primary processor for haystack server
+// the idea here is to handle the two submit types
+// but also sort out storage engines
+// I think the primary ones should be:
+// in memory, redis, dynamodb, and ssddb
+// the interface should be simple, just a read and write aspect.
