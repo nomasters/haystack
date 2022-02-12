16a2e1ed327cce404c4aa1790b280d32290ff828
Nathan Toups <n@rjrbt.io>
2022-02-11 22:52:45 -0700

    testing out entropy with secretbox

--- needle/needle_test.go
+++ needle/needle_test.go
@@ -2,18 +2,45 @@ package needle
 
 import (
 	"crypto/rand"
+	"fmt"
 	"testing"
+
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
 func TestEntropy(t *testing.T) {
-	var a Shaft
-	var b Shaft
-	r1 := entropy(a)
-	rand.Read(b[:])
 
-	r2 := entropy(b)
+	var key [32]byte
+	rand.Read(key[:])
+	message := []byte("Character is 100% free online character count calculator that's simple to use. Sometimes users prefer simplicity over all of the detailed writing information Word Counter provides, and this is exactly what this tool offers. It displays character count and word count which is often the only information a person needs to know about their writing. Best of all, you receive the needed33")
 
-	t.Log(r1)
-	t.Log(r2)
+	freq := make(map[string]uint64)
 
+	for i := 0; i < 10000; i++ {
+
+		var nonce [24]byte
+		rand.Read(nonce[:])
+		var lookup [24]byte
+		rand.Read(lookup[:])
+
+		encrypted := secretbox.Seal(nonce[:], message, &nonce, &key)
+
+		var s Shaft
+
+		copy(s[:24], lookup[:])
+		copy(s[24:], encrypted[:])
+
+		e := entropy(s)
+		k := fmt.Sprintf("%.2f", e)
+		freq[k] += 1
+	}
+
+	// for i := 0; i < 100; i++ {
+	// 	var s Shaft
+	// 	rand.Read(s[:])
+	// 	e := entropy(s)
+	// 	k := fmt.Sprintf("%.2f", e)
+	// 	freq[k] += 1
+	// }
+	t.Log(freq)
 }
