455146ca7528520d4e24a354e37cf639700004ac
Nathan Toups <n@rjrbt.io>
2022-02-12 22:04:29 -0700

    added testing ideas

--- needle/needle_test.go
+++ needle/needle_test.go
@@ -44,3 +44,9 @@ func TestEntropy(t *testing.T) {
 	// }
 	t.Log(freq)
 }
+
+// Ideas for test cases:
+// - set of pure random: should get results in expected distribution
+// - set of encrypted payloads should get the same
+// - specific results like clear text and other common byte encodes should fail entropy
+// - TODO: research common encodings and/or compression that might pass, it would be interesting
