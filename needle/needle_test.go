d6e0bb847ea552abeb1474d79e235dbd47274a85
Nathan Toups <n@rjrbt.io>
2022-02-15 23:43:08 -0700

    chore: removed comments

--- needle/needle_test.go
+++ needle/needle_test.go
@@ -123,38 +123,3 @@ func TestFromBytes(t *testing.T) {
 		}
 	}
 }
-
-// func TestEntropy(t *testing.T) {
-
-// 	freq := make(map[string]uint64)
-
-// 	for i := 0; i < 100; i++ {
-
-// 		payload := make([]byte, 448)
-// 		rand.Read(payload[:])
-
-// 		result := http.DetectContentType(payload)
-// 		freq[result]++
-// 		// _, err := New(payload[:])
-
-// 		// if err != nil {
-// 		// 	t.Error(err)
-// 		// }
-
-// 	}
-
-// 	// for i := 0; i < 100; i++ {
-// 	// 	var s Shaft
-// 	// 	rand.Read(s[:])
-// 	// 	e := entropy(s)
-// 	// 	k := fmt.Sprintf("%.2f", e)
-// 	// 	freq[k] += 1
-// 	// }
-// 	t.Log(freq)
-// }
-
-// Ideas for test cases:
-// - set of pure random: should get results in expected distribution
-// - set of encrypted payloads should get the same
-// - specific results like clear text and other common byte encodes should fail entropy
-// - TODO: research common encodings and/or compression that might pass, it would be interesting
