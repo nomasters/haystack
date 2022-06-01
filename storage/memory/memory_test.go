6efbefdb06d31826baceaa0e33840219b09c2b21
Nathan Toups <n@rjrbt.io>
2022-05-31 22:48:17 -0600

    feat: adding testing for memory store

--- storage/memory/memory_test.go
+++ storage/memory/memory_test.go
@@ -1,1 +1,13 @@
 package memory
+
+import "testing"
+
+func TestStore(t *testing.T) {
+	t.Parallel()
+	t.Run("set", func(t *testing.T) {
+		t.Parallel()
+	})
+	t.Run("get", func(t *testing.T) {
+		t.Parallel()
+	})
+}
