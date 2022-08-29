4c39335753fec46cd66e33dc804616abbbaf045f
Nathan Toups <n@rjrbt.io>
2022-08-28 23:08:22 -0600

    wip

--- server/response_test.go
+++ server/response_test.go
@@ -134,7 +134,7 @@ func TestResponse(t *testing.T) {
 	})
 }
 
-func BenchmarkNewResponse(b *testing.B) {
+func BenchmarkNewResponseBothKeys(b *testing.B) {
 	p, _ := hex.DecodeString("40e4350b03d8b0c9e340321210b259d9a20b19632929b4a219254a4269c11f820c75168c6a91d309f4b134a7d715a5ac408991e1cf9415995053cf8a4e185dae22a06617ac51ebf7d232bc49e567f90be4db815c2b88ca0d9a4ef7a5119c0e592c88dfb96706e6510fb8a657c0f70f6695ea310d24786e6d980e9b33cf2665342b965b2391f6bb982c4c5f6058b9cba58038d32452e07cdee9420a8bd7f514e1")
 	n, _ := needle.New(p)
 	h := n.Hash()
@@ -143,9 +143,21 @@ func BenchmarkNewResponse(b *testing.B) {
 	rand.Read(priv[:])
 	var preshared [64]byte
 	rand.Read(preshared[:])
+	now := time.Now()
 
 	for n := 0; n < b.N; n++ {
-		NewResponse(time.Now(), h, &preshared, &priv)
+		NewResponse(now, h, &preshared, &priv)
+	}
+}
+
+func BenchmarkNewResponseNoKeys(b *testing.B) {
+	p, _ := hex.DecodeString("40e4350b03d8b0c9e340321210b259d9a20b19632929b4a219254a4269c11f820c75168c6a91d309f4b134a7d715a5ac408991e1cf9415995053cf8a4e185dae22a06617ac51ebf7d232bc49e567f90be4db815c2b88ca0d9a4ef7a5119c0e592c88dfb96706e6510fb8a657c0f70f6695ea310d24786e6d980e9b33cf2665342b965b2391f6bb982c4c5f6058b9cba58038d32452e07cdee9420a8bd7f514e1")
+	n, _ := needle.New(p)
+	h := n.Hash()
+	now := time.Now()
+
+	for n := 0; n < b.N; n++ {
+		NewResponse(now, h, nil, nil)
 	}
 }
 
