1868035dd6ff170c0668a3d6573adfbc2d82726f
Nathan Toups <n@rjrbt.io>
2022-09-15 22:05:45 -0600

    wip: adding initial ideas for cmd

--- cmd/haystack/haystack.go
+++ cmd/haystack/haystack.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+// TODO: use this as a way to run haystack client and haystack server
+// think about what this interface might be like, but it would be useful to
+// be able to have the CLI test send/receive of messages
+
 func main() {
 	fmt.Println("hello, world")
 }
