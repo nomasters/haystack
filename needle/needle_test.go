012a6b959a05b67cdeebf03902fc20b537338ff7
Nathan Toups <n@rjrbt.io>
2022-11-08 21:23:57 -0700

    fix: fixed Bytes method test

--- needle/needle_test.go
+++ needle/needle_test.go
@@ -11,14 +11,12 @@ func TestNeedle(t *testing.T) {
 	t.Run("Bytes", func(t *testing.T) {
 		t.Parallel()
 		p, _ := hex.DecodeString("40e4350b03d8b0c9e340321210b259d9a20b19632929b4a219254a4269c11f820c75168c6a91d309f4b134a7d715a5ac408991e1cf9415995053cf8a4e185dae22a06617ac51ebf7d232bc49e567f90be4db815c2b88ca0d9a4ef7a5119c0e592c88dfb96706e6510fb8a657c0f70f6695ea310d24786e6d980e9b33cf2665342b965b2391f6bb982c4c5f6058b9cba58038d32452e07cdee9420a8bd7f514e1")
-		// expected, _ := hex.DecodeString("f82e0ca0d2fb1da76da6caf36a9d0d2838655632e85891216dc8b545d8f1410940e4350b03d8b0c9e340321210b259d9a20b19632929b4a219254a4269c11f820c75168c6a91d309f4b134a7d715a5ac408991e1cf9415995053cf8a4e185dae22a06617ac51ebf7d232bc49e567f90be4db815c2b88ca0d9a4ef7a5119c0e592c88dfb96706e6510fb8a657c0f70f6695ea310d24786e6d980e9b33cf2665342b965b2391f6bb982c4c5f6058b9cba58038d32452e07cdee9420a8bd7f514e1")
 		n, _ := New(p)
 		b := n.Bytes()
 		b[0], b[1], b[2], b[3] = 0, 0, 0, 0
-		// TODO: test that
-		// bytes return value is Payload Length
-		// bytes return value does not mutate needle
-
+		if bytes.Equal(n.Bytes(), b) {
+			t.Error("mutating Bytes() changed needle bytes")
+		}
 	})
 }
 
